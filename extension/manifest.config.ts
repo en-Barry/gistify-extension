2a09cbd3bbffba8ffa1854576415c42666a29828
en-Barry <en_barry@icloud.com>
2025-05-05 00:18:57 +0900

    feat: アイコンを追加し、マニフェストにデフォルトアイコンとアプリアイコンを設定

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -5,7 +5,17 @@ export default defineManifest({
   name: '動画より文字派！',
   version: '1.0.0',
   action: {
-    default_popup: 'extension/index.html'
+    default_popup: 'extension/index.html',
+    default_icon: {
+      '32': 'icons/icon32.png',
+      '48': 'icons/icon48.png',
+      '128': 'icons/icon128.png',
+    }
+  },
+  icons: {
+    '32': 'icons/icon32.png',
+    '48': 'icons/icon48.png',
+    '128': 'icons/icon128.png',
   },
   permissions: ['storage', 'activeTab'],
   host_permissions: ['https://www.youtube.com/*']
