59ec4354136ca806c5d3a3157219d99fb42cb52b
en-Barry <en_barry@icloud.com>
2025-05-06 14:03:32 +0900

    feat: マニフェストに新しいアイコンを追加し、不要なアイコンファイルを削除

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -4,18 +4,11 @@ export default defineManifest({
   manifest_version: 3,
   name: '動画より文字派！',
   version: '1.0.0',
+  icons: {
+    '128': 'extension/public/icon_128.png',
+  },
   action: {
     default_popup: 'extension/index.html',
-    default_icon: {
-      '32': 'icons/icon32.png',
-      '48': 'icons/icon48.png',
-      '128': 'icons/icon128.png',
-    }
-  },
-  icons: {
-    '32': 'icons/icon32.png',
-    '48': 'icons/icon48.png',
-    '128': 'icons/icon128.png',
   },
   permissions: ['storage', 'activeTab'],
 })
