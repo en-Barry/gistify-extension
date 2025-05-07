f75097be5fdedc03a5472a2d8105b07c6c9883fc
en-Barry <en_barry@icloud.com>
2025-05-08 00:48:49 +0900

    update: バージョン番号を1.0.0から1.0.1に更新

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin';
 export default defineManifest({
   manifest_version: 3,
   name: '動画より文字派！',
-  version: '1.0.0',
+  version: '1.0.1',
   icons: {
     '128': 'extension/public/icon_128.png',
   },
